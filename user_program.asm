27443d809cc230a9c6d0bb04d4c69eca9ca04dc9
Usman Farooqi <uhfarooqi@gmail.com>
2017-12-09 22:32:12 -0600

    added user input prompt loop

--- user_program.asm
+++ user_program.asm
@@ -3,6 +3,23 @@
                           ; set up the keyboard interrupt vector table entry
                   
                        ; enable keyboard interrupts
-                        ; start of actual user program to continuously print the prompt
-                   
+; ==PROMPT==
+Loop	LEA R0,prompt
+	TRAP x22
+	LD R0,lf
+	TRAP x21
+	JSR DELAY
+	BR Loop
+HALT
+DELAY   ST  R1, SaveR1
+        LD  R1, COUNT
+REP     ADD R1,R1,#-1
+        BRnp REP
+        LD  R1, SaveR1
+        RET
+COUNT   .FILL #65535
+lf	.FILL x000A
+SaveR1  .BLKW 1
+prompt .STRINGZ "Input a capital letter from the English alphabet:"
+
 .END
